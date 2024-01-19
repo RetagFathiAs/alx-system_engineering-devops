bd1daa435535464e0c441611c8e70abb97d5a8ac
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-01-19 20:50:09 +0200

    alx

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 file { '/tmp/school':
       ensure     => 'file',
       content    => 'I love Puppet',
-      permission => '0744',
+      mode ==> '0744',
       group      => 'www-data',
       owner      => 'www-data'
       }
