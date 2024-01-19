cef3e41564f44ef6bfb971bbf2295607a2388faa
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-01-19 21:33:43 +0200

    alx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,9 @@ package { 'Flask' :
           ensure   => '2.1.0',
           provider => 'pip3'
         }
+
+# Werkzeug
+package { 'Werkzeug':
+            ensure   => '2.2.1',
+            provider => 'pip3',
+          }
