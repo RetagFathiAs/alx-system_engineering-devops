14e34cfe28afee4707dd166889376a656edafdc3
RetagFathiAs <retagfathi128@gmail.com>
2024-01-19 10:46:21 -0800

    alx

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creat file
 file { '/tmp/school':
-      ensure => 'file',
-      content => 'I love Puppet',
+      ensure     => 'file',
+      content    => 'I love Puppet',
       permission => '0744',
-      group => 'www-data',
-      owner => 'www-data'
+      group      => 'www-data',
+      owner      => 'www-data'
       }
