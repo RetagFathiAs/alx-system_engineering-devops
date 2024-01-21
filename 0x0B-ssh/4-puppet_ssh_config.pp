1c9c2e39fd7d4e3c35a12f6892e9a8e78d983acb
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-01-21 23:55:38 +0200

    alx

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
-# Use puppet to make configuration SSH file access without password
+# Use puppet password
 
 exec {'/etc/ssh/ssh_config':
-  path    => '/bin',
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+      path    => '/bin',
+      command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
